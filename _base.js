8ecb869afee5fa3359fc38d8651c07679b898327
Jens Wallgren <jens-wallgren@hotmail.com>
2024-07-20 00:51:14 +0200

    Highlight selected navbar button

---
+++ _base.js
@@ -0,0 +1,15 @@
+
+// Check the current URL and highlight the correct nav button
+const url = window.location.pathname;
+const navButtons = document.querySelectorAll('.nav-button');
+
+debugger;
+navButtons.forEach(button => {
+  let underline = button.parentElement.querySelector('.underline');
+
+  if (button.getAttribute('hx-get') === url) {
+    button.classList.add('active');
+  } else {
+    button.classList.remove('active');
+  }
+});
