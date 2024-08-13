ec70ba997cebc5ad6d8f19809984ca591f1c262f
Jens Wallgren <jens-wallgren@hotmail.com>
2024-08-13 22:01:52 +0200

    Fix the js loading in the Microsoft Json Dat widget

--- templates/tools/microsoft-json-dates/script.js
+++ templates/tools/microsoft-json-dates/script.js
@@ -1,47 +1,50 @@
-  let intervalId = null;
+console.log('Microsoft JSON Dates --- script.js');
 
-  function writeJsonDate(dateObj) {
-    let unixTimestamp = Math.floor(dateObj.getTime() / 1000) * 1000;
-    document.getElementById('mjd-value').innerHTML = `/Date(${unixTimestamp})/`;
+function writeJsonDate(dateObj) {
+  let unixTimestamp = Math.floor(dateObj.getTime() / 1000) * 1000;
+  document.getElementById('mjd-value').innerHTML = `/Date(${unixTimestamp})/`;
 
-    let date = dateObj.toISOString().split('T')[0];
-    let time = dateObj.toTimeString().split(' ')[0];
-    document.getElementById('mjd-date-value').innerHTML = date;
-    document.getElementById('mjd-time-value').innerHTML = time;
+  let date = dateObj.toISOString().split('T')[0];
+  let time = dateObj.toTimeString().split(' ')[0];
+  document.getElementById('mjd-date-value').innerHTML = date;
+  document.getElementById('mjd-time-value').innerHTML = time;
+}
+
+function writeJsonDateNow() { writeJsonDate(new Date()) }
+
+
+document.body.addEventListener('htmx:load', function (e) {
+  // When page load via url: the target is the body. When via HTMX: the target is mjd-page
+  let element = e.detail.elt;
+  console.dir(element);
+  if (element.tagName === 'BODY' || element.id === 'mjd-page') {
+    startInterval();
   }
+});
 
-  let writeJsonDateNow = () => writeJsonDate(new Date());
-
-
-  document.body.addEventListener('htmx:load', function (e) {
-    // When page load via url: the target is the body. When via HTMX: the target is mjd-page
-    let element = e.detail.elt;
-    console.dir(element);
-    if (element.tagName === 'BODY' || element.id === 'mjd-page') {
-      startInterval();
-    }
-  });
-
-  let startInterval = () => {
-    writeJsonDateNow();
-    if (!intervalId) {
-      intervalId = setInterval(writeJsonDateNow, 1000);
-    }
-    document.querySelector('.mjd-button.play img').src = '/media/icons/tabler/player-stop.svg';
+function startInterval() {
+  writeJsonDateNow();
+  if (!window.intervalId) {
+    window.intervalId = setInterval(writeJsonDateNow, 1000);
   }
+  document.querySelector('.mjd-button.play img').src = '/media/icons/tabler/player-stop.svg';
+}
 
-  let stopInterval = () => {
-    clearInterval(intervalId);
-    intervalId = null;
-    document.querySelector('.mjd-button.play img').src = '/media/icons/tabler/player-play.svg';
+function stopInterval() {
+  clearInterval(window.intervalId);
+  window.intervalId = null;
+  document.querySelector('.mjd-button.play img').src = '/media/icons/tabler/player-play.svg';
+}
+
+document.body.addEventListener('htmx:beforeSwap', function (e) {
+  if (e.detail.pathInfo.requestPath === e.detail.target.id !== 'mjd-page') {
+    stopInterval();
   }
+});
 
-  document.body.addEventListener('htmx:beforeSwap', function (e) {
-    if (e.detail.pathInfo.requestPath === e.detail.target.id !== 'mjd-page') {
-      stopInterval();
-    }
-  });
+function jsonDateToClipboard() {
+  navigator.clipboard.writeText(document.getElementById('mjd-value').innerHTML);
+}
 
-  function jsonDateToClipboard() {
-    navigator.clipboard.writeText(document.getElementById('mjd-value').innerHTML);
-  }
+startInterval();
+document.querySelector('.mjd-value-widget').classList.add('show');
